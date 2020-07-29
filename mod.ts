78a2804557b5c51c27cfef4c52f9f8623ae0a1e6
daemon1024 <barun1024@gmail.com>
2020-07-29 13:26:52 +0530

    Allow only the initiator to interact with the msg

--- mod.ts
+++ mod.ts
@@ -55,76 +55,87 @@ bot.on(UpdateType.Message, async ({ message }: any) => {
 });
 
 bot.on(UpdateType.CallbackQuery, async ({ callback_query }: any) => {
-  const { id, data, message } = callback_query;
+  const { id, from, data, message } = callback_query;
   try {
-    let query = data?.split(" ");
-    if (query[0] != "back") {
-      const response = await fetch(
-        `https://pub.dev/api/packages/${query[0]}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
+    // console.log(callback_query);
+    if (from.id == message.reply_to_message.from.id) {
+      await bot.answerCallbackQuery({
+        callback_query_id: id,
+      });
+      let query = data?.split(" ");
+      if (query[0] != "back") {
+        const response = await fetch(
+          `https://pub.dev/api/packages/${query[0]}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
           },
-        },
-      );
+        );
 
-      let pkg = await response.json();
-      query.shift();
-      let text = "*Package* : `" + pkg.name + "`\n*Latest Version :* `" +
-        pkg.latest.version +
-        "`\n\n*Description : * `" + pkg.latest.pubspec.description +
-        "` \n\n*Pubspec : * `" + pkg.latest.pubspec.name + "\: \^" +
-        pkg.latest.pubspec.version + "`";
-      await bot.editMessageText({
-        chat_id: message.chat.id,
-        message_id: message.message_id,
-        text: text,
-        parse_mode: "Markdown",
-        reply_markup: {
-          inline_keyboard: [
-            [
-              { text: "Github", url: pkg.latest.pubspec.homepage },
-              { text: "Pub.dev", url: `https://pub.dev/packages/${pkg.name}` },
+        let pkg = await response.json();
+        query.shift();
+        let text = "*Package* : `" + pkg.name + "`\n*Latest Version :* `" +
+          pkg.latest.version +
+          "`\n\n*Description : * `" + pkg.latest.pubspec.description +
+          "` \n\n*Pubspec : * `" + pkg.latest.pubspec.name + "\: \^" +
+          pkg.latest.pubspec.version + "`";
+        await bot.editMessageText({
+          chat_id: message.chat.id,
+          message_id: message.message_id,
+          text: text,
+          parse_mode: "Markdown",
+          reply_markup: {
+            inline_keyboard: [
+              [
+                { text: "Github", url: pkg.latest.pubspec.homepage },
+                {
+                  text: "Pub.dev",
+                  url: `https://pub.dev/packages/${pkg.name}`,
+                },
+              ],
+              [{ text: "Back", callback_data: "back " + query.join(" ") }],
             ],
-            [{ text: "Back", callback_data: "back " + query.join(" ") }],
-          ],
-        },
-      });
-    } else {
-      query.shift();
-      const response = await fetch(
-        `https://pub.dev/api/search?q=${query.join(" ")}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
           },
-        },
-      );
-      let data = await response.json();
-      let ikb = data.packages.slice(0, 5).map((pkg: any) => {
-        return [{
-          text: pkg.package,
-          callback_data: pkg.package + " " + query.join(" "),
-        }];
-      });
-      await bot.editMessageText({
-        chat_id: message.chat.id,
-        message_id: message.message_id,
-        text: "Available packages for your search query : ",
-        reply_markup: {
-          inline_keyboard: ikb,
-        },
+        });
+      } else {
+        query.shift();
+        const response = await fetch(
+          `https://pub.dev/api/search?q=${query.join(" ")}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        );
+        let data = await response.json();
+        let ikb = data.packages.slice(0, 5).map((pkg: any) => {
+          return [{
+            text: pkg.package,
+            callback_data: pkg.package + " " + query.join(" "),
+          }];
+        });
+        await bot.editMessageText({
+          chat_id: message.chat.id,
+          message_id: message.message_id,
+          text: "Available packages for your search query : ",
+          reply_markup: {
+            inline_keyboard: ikb,
+          },
+        });
+      }
+    } else {
+      await bot.answerCallbackQuery({
+        callback_query_id: id,
+        text: "Only request initiator can interact.",
+        // show_alert:true
       });
     }
   } catch (error) {
     console.log(error);
   }
-
-  await bot.answerCallbackQuery({
-    callback_query_id: id,
-  });
 });
 
 bot.on(
